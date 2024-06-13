fd7251bf4790d65b8360f942dbb4a840e2b61bba
Drew DeCarme <drewdecarme@gmail.com>
2024-06-13 17:34:56 -0400

    working the way i want it to

--- packages/buttery-docs/.buttery-docs/_template/router.tsx
+++ packages/buttery-docs/.buttery-docs/_template/router.tsx
@@ -1,46 +1,31 @@
-import { Suspense } from "react";
-import React from "react";
 import { type RouteObject, createBrowserRouter } from "react-router-dom";
 import type { ButteryDocsGraph } from "../../commands/_utils/types";
 import { graph, header } from "./data";
 import RootRoute from "./routes/root";
 
-// import Index from "../0ca9af1273702fdea8433ff56ce114eac4dc9b061ef11ba0c1df178537dfc227/docs/_index.md";
-
-// function createModImports = (graph: ButteryDocsGraph) => {
-
-// }
-
-const mdxModules = Object.values(graph as ButteryDocsGraph).reduce(
-  (accum, graphValue) => {
-    return Object.assign(accum, {
-      [graphValue.filename]: React.lazy(
-        () => import(`./docs/${graphValue.filename}.md`)
-      ),
-    });
-  },
-  {}
-);
-
-const createRoute = (graph: ButteryDocsGraph): RouteObject[] | undefined => {
-  if (Object.entries(graph).length === 0) return undefined;
-  // @ts-ignore
-  const routes = Object.values(graph).map<RouteObject>((graphValue) => {
-    const Component = mdxModules[graphValue.filename];
-    return {
-      path: graphValue.routeRel,
-      element: Component ? (
-        <Suspense>
-          <Component />
-        </Suspense>
-      ) : (
-        <div>no component yet</div>
-      ),
-      children: createRoute(graphValue.pages),
-    };
-  });
+const createRoute = (graph: ButteryDocsGraph): RouteObject[] => {
+  const routes = Object.values(graph).reduce<RouteObject[]>(
+    (accum, graphValue) => {
+      const route = {
+        path: graphValue.routeAbs,
+        lazy: async () => {
+          const module = await import(`./docs/${graphValue.filename}.md`);
+          const Component = module.default;
+          return { Component };
+        },
+      };
+      if (Object.values(graphValue.pages).length > 0) {
+        return accum.concat(route).concat(createRoute(graphValue.pages));
+      }
+      return accum.concat(route);
+    },
+    []
+  );
   return routes;
 };
+
+const routes = createRoute(graph);
+console.log(routes);
 
 export const router = createBrowserRouter([
   {
@@ -50,6 +35,6 @@ export const router = createBrowserRouter([
       graph,
       header: header ?? null,
     }),
-    children: createRoute(graph),
+    children: routes,
   },
 ]);
