5469b38fc34fff8b648c2a7d872bd6f22b6f1a23
Drew DeCarme <drewdecarme@gmail.com>
2024-06-30 13:29:43 -0400

    remove uneeded files

--- packages/buttery-cli/.buttery/commands/docs.dev/command.ts
+++ packages/buttery-cli/.buttery/commands/docs.dev/command.ts
@@ -1,14 +1,7 @@
-import path from "node:path";
 import type { CommandAction, CommandMeta } from "../../../lib/types.js";
 import { LOG_DOCS } from "../docs/docs.logger.js";
-import { getButteryDocsFiles } from "../docs/shared.getButteryDocFiles.js";
 
-import { spawn } from "node:child_process";
-import { runCommand } from "../_utils/util.run-command.js";
 import { getButteryDocsConfig } from "../docs/shared.getButteryDocsConfig.js";
-import { getButteryDocsDirectories } from "../docs/shared.getButteryDocsDirectories.js";
-import { getButteryDocsGraph } from "../docs/shared.getButteryDocsGraph.js";
-import { orderButteryDocFiles } from "../docs/shared.orderButteryDocFiles.js";
 import { createDevServer } from "./util.dev.createDevServer.js";
 import { prepareDevDirectory } from "./util.dev.prepareDevDirectory.js";
 import { writeButteryDocsGraphDevData } from "./util.dev.writeButteryDocsGraphDevData.js";
