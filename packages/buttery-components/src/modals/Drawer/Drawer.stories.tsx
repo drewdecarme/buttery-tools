d064a0de7b2d9d1c4e0d9b9c198d678a0837450c
Drew DeCarme <drewdecarme@gmail.com>
2024-08-07 14:35:48 -0400

    tsc

--- packages/buttery-components/src/modals/Drawer/Drawer.stories.tsx
+++ packages/buttery-components/src/modals/Drawer/Drawer.stories.tsx
@@ -1,39 +1,7 @@
 import type { Meta } from "@storybook/react";
-import { Drawer } from "./Drawer";
-import { useDrawer } from "./drawer.useDrawer";
 
 const meta: Meta = {
   title: "Modals / Drawer",
 } satisfies Meta<typeof meta>;
 
 export default meta;
-
-export const RightToLeft = () => {
-  const { drawerRef, openDrawer } = useDrawer();
-
-  return (
-    <>
-      <button type="button" onClick={openDrawer}>
-        Toggle Drawer
-      </button>
-      <Drawer ref={drawerRef} dxOrientation="right-to-left">
-        This is some drawer content!
-      </Drawer>
-    </>
-  );
-};
-
-export const LeftToRight = () => {
-  const { drawerRef, openDrawer } = useDrawer();
-
-  return (
-    <>
-      <button type="button" onClick={openDrawer}>
-        Toggle Drawer
-      </button>
-      <Drawer ref={drawerRef} dxOrientation="left-to-right">
-        This is some drawer content!
-      </Drawer>
-    </>
-  );
-};
