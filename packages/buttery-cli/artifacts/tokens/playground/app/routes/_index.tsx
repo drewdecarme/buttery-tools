ef8627d4d1af5ab8d92ec85af15dab566145f747
Drew DeCarme <drewdecarme@gmail.com>
2024-09-11 09:57:45 -0400

    stabilize tokens ui

--- packages/buttery-cli/artifacts/tokens/playground/app/routes/_index.tsx
+++ packages/buttery-cli/artifacts/tokens/playground/app/routes/_index.tsx
@@ -1,15 +1,17 @@
 import type { MetaFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
     { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { name: "description", content: "Welcome to Remix!" }
   ];
 };
 
 export default function Index() {
   return (
     <div className="font-sans p-4">
+      <Link to="/test">test</Link>
       <h1 className="text-3xl">Welcome to Remix</h1>
       <ul className="list-disc mt-4 pl-6 space-y-2">
         <li>
