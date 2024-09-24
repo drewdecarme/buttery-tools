2f1fb34b835261ecfd72ec9ecd968f3510f05371
Drew DeCarme <drewdecarme@gmail.com>
2024-09-23 20:21:10 -0400

    update build

--- packages/buttery-tools/.buttery/commands/docs.build/docs.build.buildForProduction.ts
+++ packages/buttery-tools/.buttery/commands/docs.build/docs.build.buildForProduction.ts
@@ -2,32 +2,29 @@ import path from "node:path";
 import { exhaustiveMatchGuard } from "../../../utils/ts";
 
 import { cp, readdir } from "node:fs/promises";
+import { exit } from "node:process";
 import { runCommand } from "../../../utils/node/util.node.run-command";
 import { LOG } from "../_logger/util.ts.logger";
 import type { ButteryDocsConfig } from "../docs/docs.getButteryDocsConfig";
 import { getButteryDocsDirectories } from "../docs/docs.getButteryDocsDirectories";
 
 export const buildForProduction = async (config: ButteryDocsConfig) => {
-  const butteryDirs = await getButteryDocsDirectories(config);
+  const dirs = await getButteryDocsDirectories(config);
   LOG.debug("Building distribution files...");
 
   try {
     switch (config.docs.buildTarget) {
       case "cloudflare-pages": {
-        const configFile = path.resolve(
-          butteryDirs.lib.apps.generated.root,
-          "./vite.config.ts"
-        );
-        process.env.REMIX_ROOT = butteryDirs.lib.apps.generated.root;
+        process.env.REMIX_ROOT = dirs.artifacts.apps.template.root;
 
         await runCommand(
-          `npx remix vite:build --config ${configFile} --emptyOutDir --logLevel=error`
+          `npx remix vite:build --config ${dirs.output.root} --emptyOutDir --logLevel=error`
         );
 
         // Move the build to the local dist
         await cp(
-          path.resolve(butteryDirs.lib.apps.generated.root, "./build"),
-          path.resolve(butteryDirs.output.root, "./build"),
+          path.resolve(dirs.artifacts.apps.template.root, "./build"),
+          path.resolve(dirs.output.root, "./build"),
           {
             recursive: true
           }
@@ -35,16 +32,12 @@ export const buildForProduction = async (config: ButteryDocsConfig) => {
 
         // move functions to local dist
         const functionsDir = path.resolve(
-          butteryDirs.lib.apps.generated.root,
+          dirs.artifacts.apps.template.root,
           "./functions"
         );
-        await cp(
-          functionsDir,
-          path.resolve(butteryDirs.output.root, "./functions"),
-          {
-            recursive: true
-          }
-        );
+        await cp(functionsDir, path.resolve(dirs.output.root, "./functions"), {
+          recursive: true
+        });
 
         break;
       }
@@ -53,7 +46,7 @@ export const buildForProduction = async (config: ButteryDocsConfig) => {
         exhaustiveMatchGuard(config.docs.buildTarget);
     }
 
-    const filesAndDirs = await readdir(butteryDirs.output.root, {
+    const filesAndDirs = await readdir(dirs.output.root, {
       recursive: true,
       withFileTypes: true
     });
@@ -61,11 +54,12 @@ export const buildForProduction = async (config: ButteryDocsConfig) => {
     const files = filesAndDirs.filter((dirent) => dirent.isFile());
     LOG.success(`Successfully built documentation app!
 
-  Location: ${butteryDirs.output.root}
+  Location: ${dirs.output.root}
   Total Files: ${files.length}
 
-${files.reduce((accum, file) => accum.concat(`    - ${path.relative(butteryDirs.output.root, `${file.parentPath}/${file.name}`)}\n`), "")}      
+${files.reduce((accum, file) => accum.concat(`    - ${path.relative(dirs.output.root, `${file.parentPath}/${file.name}`)}\n`), "")}      
 `);
+    exit();
   } catch (error) {
     throw LOG.fatal(new Error(error as string));
   }
