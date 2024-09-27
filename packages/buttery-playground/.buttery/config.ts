66bf3e8dbbb8aa9cb9604c01d426c64a1adaaf0f
Drew DeCarme <drewdecarme@gmail.com>
2024-09-27 14:34:12 -0400

    kinda working

---
+++ packages/buttery-playground/.buttery/config.ts
@@ -0,0 +1,8 @@
+import type { ButteryConfig } from "@buttery/tools/config"
+const config: ButteryConfig = {
+  "docs": {
+    "buildTarget": "cloudflare-pages",
+    "routeStrategy": "section-folders"
+  }
+};
+export default config
