0671e8b5917bacc0bf2876a5d98d49fbeca004e0
Drew DeCarme <drewdecarme@gmail.com>
2024-09-30 21:03:37 -0400

    register all dependencies of a file

--- packages/buttery-tools/.buttery/commands/components.export/command.ts
+++ packages/buttery-tools/.buttery/commands/components.export/command.ts
@@ -1,6 +1,8 @@
+import { readdirSync } from "node:fs";
 import { readdir } from "node:fs/promises";
 import path from "node:path";
 import { exit } from "node:process";
+import { traverse } from "@babel/core";
 import { printAsBullets } from "@buttery/logger";
 import { select } from "@inquirer/prompts";
 import type {
@@ -10,7 +12,7 @@ import type {
 } from "../../../lib/commands/butter-commands.types";
 import { getButteryConfig } from "../../../lib/config/getButteryConfig";
 import { LOG_CLI } from "../../../lib/logger/loggers";
-import { getAllDependenciesOfFile } from "./components.export.getAllDependenciesOfFile";
+import { parseFile } from "./components.export.parseFile";
 
 export const meta: CommandMeta = {
   name: "export",
@@ -29,21 +31,24 @@ export const options: CommandOptions<{ outDir: string }> = {
 
 export const action: CommandAction<typeof options> = async ({ options }) => {
   // TODO: Define a configuration in the buttery config to reconcile this.
-  if (!options.outDir) {
-    LOG_CLI.error("The option --outDir,-o is missing.");
-    throw LOG_CLI.fatal(new Error("An '--outDir,-o' option is required."));
-  }
+  // if (!options.outDir) {
+  //   LOG_CLI.error("The option --outDir,-o is missing.");
+  //   throw LOG_CLI.fatal(new Error("An '--outDir,-o' option is required."));
+  // }
 
   LOG_CLI.debug("Running `buttery.components.export` command");
 
   const config = await getButteryConfig("docs");
 
   LOG_CLI.debug("Getting components directory...");
-  const componentsDir = path.resolve(config.paths.rootDir, "./lib/components");
-  LOG_CLI.debug("Getting components directory... done", { componentsDir });
+  const rootComponentsDir = path.resolve(
+    config.paths.rootDir,
+    "./lib/components"
+  );
+  LOG_CLI.debug("Getting components directory... done", { rootComponentsDir });
 
   LOG_CLI.debug("Fetching components to pick from...");
-  const componentFilesAndDirs = await readdir(componentsDir, {
+  const componentFilesAndDirs = await readdir(rootComponentsDir, {
     withFileTypes: true
   });
   const componentDirs = componentFilesAndDirs.filter((dirent) =>
@@ -64,12 +69,7 @@ export const action: CommandAction<typeof options> = async ({ options }) => {
       name: dirent.name,
       value: {
         name: dirent.name,
-        path: path.join(dirent.parentPath, dirent.name),
-        mainFilePath: path.join(
-          dirent.parentPath,
-          dirent.name,
-          dirent.name.concat(".tsx")
-        )
+        path: path.join(dirent.parentPath, dirent.name)
       }
     }))
   });
@@ -78,15 +78,66 @@ export const action: CommandAction<typeof options> = async ({ options }) => {
   });
 
   // Initialize a graph
-  LOG_CLI.debug("Creating component dependency graph...");
+  LOG_CLI.debug("Searching for and registering intra-dependencies...");
   LOG_CLI.debug(componentForExport.path);
   const dependencies = new Set<string>();
 
-  getAllDependenciesOfFile(
-    dependencies,
-    componentForExport.path,
-    componentForExport.name
-  );
+  // An inline recursive function that will recursively search through all directories
+  // and look for any dependencies. All dependencies are identified with `@BUTTERY_COMPONENT`.
+  const DEPENDENCY_ID = "@BUTTERY_COMPONENT/";
+
+  function registerComponentIntraDependencies(directoryPath: string) {
+    const directoryContents = readdirSync(directoryPath, {
+      withFileTypes: true
+    });
+    for (const dirent of directoryContents) {
+      // If the contents is a directory, then recurse the function again.
+      // At this point we're not including any examples in the output.
+      if (dirent.isDirectory() && dirent.name !== "examples") {
+        const innerDirectoryPath = path.join(dirent.parentPath, dirent.name);
+        registerComponentIntraDependencies(innerDirectoryPath);
+      }
+
+      // Since it's a file, we want to turn it into an abstract syntax tree
+      // and then parse it to read the imports. This way to can start to determine
+      // if the the imports have other intra-dependencies.
+      if (dirent.isFile()) {
+        const innerFilePath = path.join(dirent.parentPath, dirent.name);
+        LOG_CLI.debug(`Registering dependencies in: ${innerFilePath}`);
+        const ast = parseFile(innerFilePath);
+        traverse(ast, {
+          ImportDeclaration: ({ node }) => {
+            const importPath = node.source.value;
+            if (
+              importPath.startsWith(DEPENDENCY_ID) &&
+              !dependencies.has(importPath) // prevents unnecessary traversal for deps already registered
+            ) {
+              const innerDependencyFileName =
+                importPath.split(DEPENDENCY_ID)[1];
+              dependencies.add(innerDependencyFileName);
+
+              // we know this directory since our component directory always starts at the root dir.
+              // the alias is nested directly under the root dir so we can assume that the inner dependency
+              // directory is at the rootComponentDir + the innerDependencyFileName
+              const innerDependencyDir = path.resolve(
+                rootComponentsDir,
+                innerDependencyFileName
+              );
+
+              // recursively register other intra-dependencies in the other component directory.
+              registerComponentIntraDependencies(innerDependencyDir);
+            }
+          }
+        });
+      }
+    }
+  }
+
+  // use the inline recursive function to start searching for intra-dependencies
+  // starting at the folder of the component selected by the user.
+  registerComponentIntraDependencies(componentForExport.path);
+
+  LOG_CLI.debug("Searching for and registering intra-dependencies... done");
 
   const dependenciesArr = [...dependencies.values()];
   if (dependenciesArr.length === 0) {
