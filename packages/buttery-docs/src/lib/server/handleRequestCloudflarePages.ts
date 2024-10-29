4ff872c66a47f57351303e36b67eb9bf41583b27
Drew DeCarme <drewdecarme@gmail.com>
2024-10-29 15:21:44 -0400

    update cf handler

--- packages/buttery-docs/src/lib/server/handleRequestCloudflarePages.ts
+++ packages/buttery-docs/src/lib/server/handleRequestCloudflarePages.ts
@@ -31,67 +31,73 @@ export async function handleRequestCloudflarePages(
   // Get the route name that we're attempting to request
   const { pathname } = new URL(context.request.url);
 
-  // Handle any assets
-  if (pathname.startsWith("/assets") || pathname.startsWith("/favicon")) {
+  // Get only the route paths
+  const routes = Object.values(bManifest).map(
+    (manifestEntry) => manifestEntry.routePath
+  );
+
+  // if the pathname is a route, then let's render the app
+  if (routes.includes(pathname)) {
     try {
-      return await context.env.ASSETS.fetch(context.request);
+      // match the buttery route with the request.pathname
+      const routeId = getButteryRouteIdFromRequest(pathname, bManifest);
+
+      // get the css and js assets from the vite manifest
+      const { cssAssets, jsAssets } = getRouteAssets(routeId, vManifest);
+
+      // Instantiate a new Meta class to collect the meta tags for each page
+      const Meta = new ButteryMeta();
+
+      // Insert the assets into the HTML template start and end
+      const { htmlStart, htmlEnd } = generateHTMLTemplate({
+        cssLinks: cssAssets,
+        jsScripts: jsAssets,
+        Meta,
+      });
+
+      const responseStatusCode = 200;
+      const responseHeaders = new Headers();
+      const encoder = new TextEncoder();
+
+      // Generate the stream using the `render` function from the server bundle
+      const context: ButteryDocsContext = { route: pathname, Meta };
+      const streamBody = await render(context, responseStatusCode);
+
+      // Insert the app body into the HTML shell
+      const stream = new ReadableStream({
+        async start(controller) {
+          // 1. Send the head and opening body tags
+          controller.enqueue(encoder.encode(htmlStart));
+
+          // 2. Pipe the SSR stream into the response
+          const reader = streamBody.getReader();
+          while (true) {
+            const { done, value } = await reader.read();
+            if (done) break;
+            controller.enqueue(value); // Send each chunk of React's stream
+          }
+
+          // 3. End with the closing body and html tags
+          controller.enqueue(encoder.encode(htmlEnd));
+          controller.close();
+        },
+      });
+
+      return new Response(stream, {
+        headers: responseHeaders,
+        status: responseStatusCode,
+      });
     } catch (error) {
-      console.error(`Error serving static file: ${pathname}`, error);
-      return new Response("Not Found", { status: 404 });
+      console.error("Error during SSR:", error);
+      return new Response("Internal Server Error", { status: 500 });
     }
   }
 
   try {
-    // match the buttery route with the request.pathname
-    const routeId = getButteryRouteIdFromRequest(pathname, bManifest);
-
-    // get the css and js assets from the vite manifest
-    const { cssAssets, jsAssets } = getRouteAssets(routeId, vManifest);
-
-    // Instantiate a new Meta class to collect the meta tags for each page
-    const Meta = new ButteryMeta();
-
-    // Insert the assets into the HTML template start and end
-    const { htmlStart, htmlEnd } = generateHTMLTemplate({
-      cssLinks: cssAssets,
-      jsScripts: jsAssets,
-      Meta,
-    });
-
-    const responseStatusCode = 200;
-    const responseHeaders = new Headers();
-    const encoder = new TextEncoder();
-
-    // Generate the stream using the `render` function from the server bundle
-    const context: ButteryDocsContext = { route: pathname, Meta };
-    const streamBody = await render(context, responseStatusCode);
-
-    // Insert the app body into the HTML shell
-    const stream = new ReadableStream({
-      async start(controller) {
-        // 1. Send the head and opening body tags
-        controller.enqueue(encoder.encode(htmlStart));
-
-        // 2. Pipe the SSR stream into the response
-        const reader = streamBody.getReader();
-        while (true) {
-          const { done, value } = await reader.read();
-          if (done) break;
-          controller.enqueue(value); // Send each chunk of React's stream
-        }
-
-        // 3. End with the closing body and html tags
-        controller.enqueue(encoder.encode(htmlEnd));
-        controller.close();
-      },
-    });
-
-    return new Response(stream, {
-      headers: responseHeaders,
-      status: responseStatusCode,
-    });
+    const asset = await context.env.ASSETS.fetch(context.request);
+    return asset;
   } catch (error) {
-    console.error("Error during SSR:", error);
-    return new Response("Internal Server Error", { status: 500 });
+    console.error(`Error serving static file: ${pathname}`, error);
+    return new Response("Not Found", { status: 404 });
   }
 }
