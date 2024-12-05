5ac0ed5d22e4ed60a9b523f64d90d0c8b5c2c34f
Drew DeCarme <drewdecarme@gmail.com>
2024-12-05 14:16:31 -0500

    add utils

--- packages/buttery-utils/src/isomorphic/index.ts
+++ packages/buttery-utils/src/isomorphic/index.ts
@@ -2,3 +2,4 @@ export * from "./util.isomorphic.exhaustive-match-guard.js";
 export * from "./util.isomorphic.generate-guid.js";
 export * from "./util.isomorphic.kebab-to-camel.js";
 export * from "./util.isomorphic.kebab-to-pascal-case.js";
+export * from "./util.isomorphic.try-handle.js";
